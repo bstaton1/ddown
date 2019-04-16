88de0c62dc1830bf41b51f87b8642ae6bb9ea13f
Ben Staton <statonbe@gmail.com>
2019-04-16 13:13:29 -0500

    clean up use of packages
    Remove dependency of StatonMisc - it was used only once for prettify() in ch4. I just added a prettify() function to _source-before-chapter.R

--- _source-before-chapter.R
+++ _source-before-chapter.R
@@ -1,15 +1,21 @@
 
 # this script gets sourced prior to knitting each chapter
 
+# load packages: these are all for the knitr::kable(...) %>% kableExtra::...() table workflow
 library(dplyr)
 library(knitr)
 library(kableExtra)
 
-# clear the workspace
+# clear the workspace: start each chapter fresh
 rm(list = ls(all = T))
 
 # set global RMarkdown chunk options: never print code, output, or warnings/messages
-knitr::opts_chunk$set(echo = F, message = F, warning = F, fig.align = "center")
+knitr::opts_chunk$set(
+  echo = F,              # never print code
+  message = F,           # never print messages
+  warning = F,           # never print warnings
+  fig.align = "center"  # center figures
+)
 
 # function to help split cells in tables
 add_break = function(x) {
@@ -18,4 +24,9 @@ add_break = function(x) {
   x[,2] = paste("(", x[,2], sep = "")
   x = apply(x, 1, function(z) paste(z, collapse = "\n"))
   kableExtra::linebreak(x, align = "c")
+}
+
+# function to insert commas in large numbers
+prettify = function (x) {
+  prettyNum(x, big.mark = ",", scientific = F)
 }
